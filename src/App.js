f50608a87904ed90b36b8b3857c9156a93b72756
Siddhant <sid.rac125@gmail.com>
2020-08-06 00:10:44 +0800

    complete front end visualisation and user input

--- src/App.js
+++ src/App.js
@@ -1,24 +1,71 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import BarChart from "react-bar-chart";
+
+import "./App.css";
 
 function App() {
+  const [data, setData] = useState([
+    { text: "Man", value: 500 },
+    { text: "Woman", value: 300 },
+  ]);
+
+  const [date, setDate] = useState("");
+  const [count, setCount] = useState(NaN);
+
+  const margin = { top: 20, right: 20, bottom: 30, left: 40 };
+
+  const handleBarClick = (element, id) => {
+    console.log(`The bin ${element.text} with id ${id} was clicked`);
+  };
+
+  const handleDateInput = (e) => {
+    setDate(e.target.value);
+  };
+
+  const handleCountInput = (e) => {
+    setCount(parseInt(e.target.value, 10));
+  };
+
+  const handleDataSubmit = (e) => {
+    if (isNaN(count)) {
+      alert("Invalid Count format! It should be a positive integer");
+      e.preventDefault();
+      return;
+    }
+    let newData = [...data];
+    newData.push({
+      text: date,
+      value: count,
+    });
+
+    console.log(newData);
+
+    setData(newData);
+    e.preventDefault();
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>React Graph Visualiser</h1>
+      <div style={{ width: "50%" }}>
+        <BarChart
+          ylabel="Quantity"
+          width={500}
+          height={500}
+          margin={margin}
+          data={data}
+          onBarClick={handleBarClick}
+        />
+      </div>
+      <form onSubmit={handleDataSubmit}>
+        <label>
+          Date <input type="text" onChange={handleDateInput}></input>
+        </label>
+        <label>
+          Count <input type="text" onChange={handleCountInput}></input>
+        </label>
+        <input type="Submit" value="Add Data"></input>
+      </form>
     </div>
   );
 }
