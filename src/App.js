10206b5d6b572f89a872bcae350bbf3510867fa9
Siddhant <sid.rac125@gmail.com>
2020-08-06 01:13:00 +0800

    complete data fetch from backend

--- src/App.js
+++ src/App.js
@@ -1,17 +1,35 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import BarChart from "react-bar-chart";
 
 import "./App.css";
 
 function App() {
-  const [data, setData] = useState([
-    { text: "Man", value: 500 },
-    { text: "Woman", value: 300 },
-  ]);
+  const [data, setData] = useState([]);
 
   const [date, setDate] = useState("");
   const [count, setCount] = useState(NaN);
 
+  useEffect(() => {
+    fetch("http://localhost:3001/getData")
+      .then((response) => response.json())
+      .then((responseJson) => {
+        // convert data from api to data required by graph object
+        var tempData = responseJson.map((object) => {
+          return {
+            text: object.date,
+            value: object.count,
+          };
+        });
+
+        console.log(tempData);
+        setData(tempData);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []); // to avoid tracking changes and thus avoid getting into infinite render
+
+  // measurement for the graph window
   const margin = { top: 20, right: 20, bottom: 30, left: 40 };
 
   const handleBarClick = (element, id) => {
@@ -50,8 +68,8 @@ function App() {
       <div style={{ width: "50%" }}>
         <BarChart
           ylabel="Quantity"
-          width={500}
-          height={500}
+          width={600}
+          height={600}
           margin={margin}
           data={data}
           onBarClick={handleBarClick}
